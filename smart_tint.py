5e943c30672da69cf3b90e253dac9fcda4e48faf
Jonah Rubin <jonah.n.rubin@gmail.com>
2018-10-31 15:50:44 -0400

    Relay working with photocell

--- smart_tint.py
+++ smart_tint.py
@@ -53,12 +53,15 @@ try:
         # if photcell val is less than threshold but relay off: switch
         if current_val < threshold:
             if not relay_on:
-                relay_on = turn_relay_on(relay_pin)
+                turn_relay_on(relay_pin)
+                time.sleep(1)
 
         # if photcell val is greater than threshold but relay on: switch
         else:
             if relay_on:
-                relay_on = turn_relay_off(relay_pin)
+                turn_relay_off(relay_pin)
+                time.sleep(1)
+
 
 
 except KeyboardInterrupt:
