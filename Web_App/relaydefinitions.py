9836324ab3b1874b5f12f724e03f563c14725262
JNR122 <jonah.n.rubin@gmail.com>
2018-11-09 23:45:26 -0500

    Adding photocell logic

--- Web_App/relaydefinitions.py
+++ Web_App/relaydefinitions.py
@@ -1,9 +1,9 @@
 relays = [
     { 'id' : 1, 'name' : 'Smart Tint', 'state' : 'off'},
-    { 'id' : 2, 'name' : 'Photocell', 'state' : 'off'},
-    ]
+    { 'id' : 2, 'name' : 'Photocell', 'state' : 'off'}
+]
 
 relayIdToPin = {
     1 : 21,
-    2 : 4,
-    }
+    2 : 4
+}
