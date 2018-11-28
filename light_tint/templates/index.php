dd3e56575dbace6e99b3eb7584699fdd9818e9c5
joshuachilds <jhchilds@uvm.edu>
2018-11-28 13:38:07 -0500

    fixing path errors deleting PHP

--- light_tint/templates/index.php
+++ light_tint/templates/index.php
@@ -1,7 +1,81 @@
-<?php
-    include("../top.php");
-    include("../header.php");
-?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr" ng-app="myApp">
+  <head>
+    <meta charset="utf-8">
+     <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+
+     <meta name="author" content="Joshua Childs">
+     <meta name="description" content="A comprehensive website/application for the 2018 UVM CS Fair.
+           This site uses a RPi to turn off and on Smart Tint with a photocell." >
+     <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+     <link rel="stylesheet" href="css/custom.css" >
+
+     <!-- %%%%%%%%%%%% LINKS FOR WEB APP %%%%%%%%%%%%-->
+     <script data-require="angular.js@1.4.8" data-semver="1.4.8" src="https://code.angularjs.org/1.4.8/angular.js"></script>
+     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.css" /> -->
+     <script src="../static/Controller.js"></script>
+     <link rel="stylesheet" href="/static/style.css" />
+     <!-- %%%%%%%%%%%% LINKS FOR WEB APP %%%%%%%%%%%%-->
+
+     <title>Light Tint</title>
+  </head>
+
+
+
+  <body>
+
+    <header>
+
+      <nav class = "navbar navbar-expand-lg navbar-light">
+
+            <a href = "index.php" class = "navbar-brand ml-3 "> <span style = "color:black;">LIGH</span><span style = "color:rgb(0,100,250);">T</span><span style = "color:black;">INT</span> </a>
+
+            <button class = "navbar-toggler" type="button" data-toggle="collapse" data-target = "#navbarMenu"
+            aria-controls="navbarMenu" aria-expanded = "false" aria-label="Toggle Navigation">
+
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- <div class="collapse navbar-collapse"></div>  THIS "CENTERS THE NAV"-->
+            <!-- Below is code for the collapsable navbarMenu, try resizing page so you you can see the "hamburger" button.-->
+            <div class="collapse navbar-collapse" id="navbarMenu">
+              <ul class="navbar-nav mr-auto" >
+                <li class="nav-item active">
+                  <a href="index.php" class="nav-link">Home </a>
+                </li>
+                <li class="nav-item">
+                  <a href="new_index.php" class="nav-link">Application</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">Code</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">References</a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">Contact Us</a>
+                </li>
+
+              </ul>
+
+              <form action = "new_index.php" method="get" class="form-inline my-2 my-lg-0">
+                <button class="btn menu-right-btn border" type="submit">
+                  Application
+                </button>
+              </form>
+
+            </div>
+
+          </nav>
+
+
+    </header>
+
+
   <main>
 
     <div class ="container-fluid p-0" id="top" >
