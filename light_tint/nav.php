652eafaa4747d942d73de86c3aea8a4089c847cd
joshuachilds <jhchilds@uvm.edu>
2018-11-28 13:29:08 -0500

    changing paths putting index.php into templates directory.

--- light_tint/nav.php
+++ light_tint/nav.php
@@ -16,7 +16,7 @@
             <a href="index.php" class="nav-link">Home </a>
           </li>
           <li class="nav-item">
-            <a href="templates/new_index.php" class="nav-link">Application</a>
+            <a href="new_index.php" class="nav-link">Application</a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">Code</a>
@@ -30,7 +30,7 @@
 
         </ul>
 
-        <form action = "templates/new_index.php" method="get" class="form-inline my-2 my-lg-0">
+        <form action = "new_index.php" method="get" class="form-inline my-2 my-lg-0">
           <button class="btn menu-right-btn border" type="submit">
             Application
           </button>
--- light_tint/top.php
+++ light_tint/top.php
@@ -11,12 +11,12 @@
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
      <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-     <link rel="stylesheet" href="css/custom.css" >
+     <link rel="stylesheet" href="../css/custom.css" >
 
      <!-- %%%%%%%%%%%% LINKS FOR WEB APP %%%%%%%%%%%%-->
      <script data-require="angular.js@1.4.8" data-semver="1.4.8" src="https://code.angularjs.org/1.4.8/angular.js"></script>
      <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.css" /> -->
-     <script src="/static/Controller.js"></script>
+     <script src="../static/Controller.js"></script>
      <link rel="stylesheet" href="/static/style.css" />
      <!-- %%%%%%%%%%%% LINKS FOR WEB APP %%%%%%%%%%%%-->
 
