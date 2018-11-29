9c5b4989981a92e103bee6d15c45a14230e47390
joshuachilds <jhchilds@uvm.edu>
2018-11-29 17:34:29 -0500

    changed paths to static..duhhh

--- light_tint/templates/index.php
+++ light_tint/templates/index.php
@@ -413,9 +413,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/scrollreveal"></script>
-    <script src="javascript/smooth_scrolling.js"></script>
-    <script src="javascript/jquery-3.3.1.min.js"></script>
-    <!-- <script src="../javascript/redirect.js"></script> -->
+    <script src="../static/javascript/smooth_scrolling.js"></script>
+    <script src="../static/javascript/jquery-3.3.1.min.js"></script>
 
 
 
